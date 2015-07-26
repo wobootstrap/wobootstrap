ebbac320ff007a7fb8a4a362723a27ef3cbad1ec
wobootstrap <bhoot.biswas@gmail.com>
2015-07-26 15:09:22 +0600

    Added few HTML markup

--- index.php
+++ index.php
@@ -12,33 +12,52 @@
      */
     get_header();
 ?>
-<div id="primary" class="content-area">
-    <main id="main" class="site-main" role="main">
-        <?php if ( have_posts() ) : ?>
-            <?php /* Start the Loop */ ?>
-            <?php while ( have_posts() ) : the_post(); ?>
-                <?php
-                    /*
-                    * Include the Post-Format-specific template for the content.
-                    * If you want to override this in a child theme, then include a file
-                    * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-                    */
-                    get_template_part( 'template-parts/content', get_post_format() );
-                ?>
-            <?php endwhile; ?>
-            <?php the_posts_navigation(); ?>
-            <?php if ( is_singular() ) : // If viewing a single post/page/CPT. ?>
-                <?php
-                    // If comments are open or we have at least one comment, load up the comment template.
-                    if ( comments_open() || get_comments_number() ) :
-                        comments_template();
-                    endif;
-                ?>
-            <?php endif; // End check for single post. ?>
-        <?php else : ?>
-            <?php get_template_part( 'template-parts/content', 'none' ); ?>
-        <?php endif; ?>
-    </main><!-- #main -->
-</div><!-- #primary -->
-<?php get_sidebar(); ?>
+<main id="main" class="site-main" role="main">
+    <?php if ( have_posts() ) : ?>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8">
+                <div id="primary" class="content-area">
+                    <?php /* Start the Loop */ ?>
+                    <?php while ( have_posts() ) : the_post(); ?>
+                    <?php
+                        /*
+                        * Include the Post-Format-specific template for the content.
+                        * If you want to override this in a child theme, then include a file
+                        * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+                        */
+                        get_template_part( 'template-parts/content', get_post_format() );
+                    ?>
+                    <?php endwhile; ?>
+                    <?php the_posts_navigation(); ?>
+                    <?php if ( is_singular() ) : // If viewing a single post/page/CPT. ?>
+                    <?php
+                        // If comments are open or we have at least one comment, load up the comment template.
+                        if ( comments_open() || get_comments_number() ) :
+                            comments_template();
+                        endif;
+                    ?>
+                    <?php endif; // End check for single post. ?>
+                </div><!-- #primary -->
+            </div>
+            <div class="col-md-4">
+                <?php get_sidebar(); ?>
+            </div>
+        </div>
+    </div>
+    <?php else : ?>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8">
+                <div id="primary" class="content-area">
+                    <?php get_template_part( 'template-parts/content', 'none' ); ?>
+                </div><!-- #primary -->
+            </div>
+            <div class="col-md-4">
+                <?php get_sidebar(); ?>
+            </div>
+        </div>
+    </div>    
+    <?php endif; ?>
+</main><!-- #main -->
 <?php get_footer(); ?>
