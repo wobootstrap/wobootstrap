5bd871d0b49bc31acc01d0be7927baf0dc104125
wobootstrap <bhoot.biswas@gmail.com>
2015-07-26 14:52:42 +0600

    Fixed 'bootstrap-js' dir uri

--- functions.php
+++ functions.php
@@ -116,7 +116,7 @@ function wobootstrap_scripts() {
 
 	wp_enqueue_style( 'wobootstrap-style', get_stylesheet_uri() );
 
-    wp_enqueue_script( 'bootstrap-js', get_template_directory_uri() . 'js/bootstrap.min.js', array( 'jquery' ), '3.3.5', true );
+    wp_enqueue_script( 'bootstrap-js', get_template_directory_uri() . '/js/bootstrap.min.js', array( 'jquery' ), '3.3.5', true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
