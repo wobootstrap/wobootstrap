bfc23d056c69091dda9c76fe5c60058b304110f0
wobootstrap <bhoot.biswas@gmail.com>
2015-07-26 15:11:32 +0600

    Better HTML markup

--- footer.php
+++ footer.php
@@ -1,26 +1,21 @@
 <?php
-/**
- * The template for displaying the footer.
- *
- * Contains the closing of the #content div and all content after
- *
- * @package WoBootstrap
- */
-
+    /**
+     * The template for displaying the footer.
+     *
+     * Contains the closing of the #content div and all content after
+     *
+     * @package WoBootstrap
+     */
 ?>
-
-	</div><!-- #content -->
-
-	<footer id="colophon" class="site-footer" role="contentinfo">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'wobootstrap' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'wobootstrap' ), 'WordPress' ); ?></a>
-			<span class="sep"> | </span>
-			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'wobootstrap' ), 'wobootstrap', '<a href="http://underscores.me/" rel="designer">Underscores.me</a>' ); ?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
-</div><!-- #page -->
-
-<?php wp_footer(); ?>
-
-</body>
+            </div><!-- #content -->
+            <footer id="colophon" class="site-footer" role="contentinfo">
+                <div class="site-info">
+                    <a href="<?php echo esc_url( __( 'https://wordpress.org/', 'wobootstrap' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'wobootstrap' ), 'WordPress' ); ?></a>
+                    <span class="sep"> | </span>
+                    <?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'wobootstrap' ), 'wobootstrap', '<a href="http://underscores.me/" rel="designer">Underscores.me</a>' ); ?>
+                </div><!-- .site-info -->
+            </footer><!-- #colophon -->
+        </div><!-- #page -->
+        <?php wp_footer(); ?>
+    </body>
 </html>
