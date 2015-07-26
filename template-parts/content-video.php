c4e5e00a3d79d9eea0acaa118fd81ec8778e91e0
wobootstrap <bhoot.biswas@gmail.com>
2015-07-26 21:36:48 +0600

    Added 'hybrid_media_grabber' func

--- template-parts/content-video.php
+++ template-parts/content-video.php
@@ -6,42 +6,46 @@
      */
 ?>
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-
-    <?php if ( is_single( get_the_ID() ) ) : // If viewing a single post. ?>
-    <header class="entry-header">
-        <?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
-        <div class="entry-meta">
-            <?php wobootstrap_posted_on(); ?>
-        </div><!-- .entry-meta -->
-    </header><!-- .entry-header -->
-    <div class="entry-content">
-        <?php the_content(); ?>
-        <?php wp_link_pages(); ?>
-    </div><!-- .entry-content -->
-    <footer class="entry-footer">
-        <?php wobootstrap_entry_footer(); ?>
-    </footer><!-- .entry-footer -->
-    <?php else : // If not viewing a single post. ?>
-    <header class="entry-header">
-        <?php the_title( sprintf( '<h1 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h1>' ); ?>
-        <?php if ( 'post' == get_post_type() ) : ?>
-        <div class="entry-meta">
-            <?php wobootstrap_posted_on(); ?>
-        </div><!-- .entry-meta -->
-        <?php endif; ?>
-    </header><!-- .entry-header -->
-    <?php if ( has_excerpt() ) : // If the post has an excerpt. ?>
-    <div class="entry-summary">
-        <?php the_excerpt(); ?>
-    </div><!-- .entry-summary -->
-    <?php elseif ( empty( $video ) ) : // Else, if the post does not have a video. ?>
-    <div class="entry-content">
-        <?php the_content(); ?>
-    </div><!-- .entry-content -->
-    <?php endif; // End excerpt/video checks. ?>
-    <footer class="entry-footer">
-        <?php wobootstrap_entry_footer(); ?>
-    </footer><!-- .entry-footer -->
-    <?php endif; // End single post check. ?>
-
+    <?php echo ( $video = hybrid_media_grabber( array( 'width' => 1100, 'type' => 'video', 'split_media' => true, 'before' => '<div class="entry-media">', 'after' => '</div>' ) ) ); ?>
+    <div class="entry-wrap">
+        <?php if ( is_single( get_the_ID() ) ) : // If viewing a single post. ?>
+        <header class="entry-header">
+            <?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+        </header><!-- .entry-header -->
+        <div class="entry-content">
+            <?php the_content(); ?>
+            <?php
+                wp_link_pages( array(
+                    'before' => '<div class="page-links">' . esc_html__( 'Pages:', '_s' ),
+                    'after'  => '</div>',
+                ) );
+            ?>
+        </div><!-- .entry-content -->
+        <footer class="entry-footer">
+            <?php wobootstrap_entry_footer(); ?>
+        </footer><!-- .entry-footer -->
+        <?php else : // If not viewing a single post. ?>
+        <header class="entry-header">
+            <?php the_title( sprintf( '<h1 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h1>' ); ?>
+        </header><!-- .entry-header -->
+        <?php if ( has_excerpt() ) : // If the post has an excerpt. ?>
+        <div class="entry-summary">
+            <?php the_excerpt(); ?>
+        </div><!-- .entry-summary -->
+        <?php elseif ( empty( $video ) ) : // Else, if the post does not have audio. ?>
+        <div class="entry-content">
+            <?php the_content(); ?>
+            <?php
+                wp_link_pages( array(
+                    'before' => '<div class="page-links">' . esc_html__( 'Pages:', '_s' ),
+                    'after'  => '</div>',
+                ) );
+            ?>
+        </div><!-- .entry-content -->
+        <?php endif; // End excerpt/audio checks. ?>
+        <footer class="entry-footer">
+            <?php wobootstrap_entry_footer(); ?>
+        </footer><!-- .entry-footer -->
+        <?php endif; // End single post check. ?>
+    </div>
 </article><!-- #post-## -->
