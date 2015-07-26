571555cac56ca69293ba062d4cbe740dc80a4464
wobootstrap <bhoot.biswas@gmail.com>
2015-07-26 12:54:57 +0600

    Updated meta tags

--- header.php
+++ header.php
@@ -10,7 +10,9 @@
 <html <?php language_attributes(); ?>>
     <head>
         <meta charset="<?php bloginfo( 'charset' ); ?>">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
         <link rel="profile" href="http://gmpg.org/xfn/11">
         <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
         <?php wp_head(); ?>
