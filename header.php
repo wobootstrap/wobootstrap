1803cf39e1aa27d73904dd0852a885f1371eb77b
wobootstrap <bhoot.biswas@gmail.com>
2015-07-26 12:56:47 +0600

    Added HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries

--- header.php
+++ header.php
@@ -16,6 +16,12 @@
         <link rel="profile" href="http://gmpg.org/xfn/11">
         <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
         <?php wp_head(); ?>
+        <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
+            <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+            <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+        <![endif]-->
     </head>
     <body <?php body_class(); ?>>
         <div id="page" class="hfeed site">
