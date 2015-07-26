0c6622dc2db5cc0619520ae86ec2e6f1012848a6
wobootstrap <bhoot.biswas@gmail.com>
2015-07-26 14:00:44 +0600

    Added navabr-right class to menu_class

--- header.php
+++ header.php
@@ -48,7 +48,7 @@
                                  'container'         => 'div',
                                  'container_class'   => 'collapse navbar-collapse',
                                  'container_id'      => 'bs-example-navbar-collapse-1',
-                                 'menu_class'        => 'nav navbar-nav',
+                                 'menu_class'        => 'nav navbar-nav navbar-right',
                                  'fallback_cb'       => 'wp_bootstrap_navwalker::fallback',
                                  'walker'            => new wp_bootstrap_navwalker())
                             );
