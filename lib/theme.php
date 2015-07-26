39c0641ad514bf257304a5a650e6c6cf8d9f8080
wobootstrap <bhoot.biswas@gmail.com>
2015-07-26 13:37:01 +0600

    Added theme.php file
    Sets up custom filters and actions for the theme.  This does things like
    sets up sidebars, menus, scripts, and lots of other awesome stuff that
    WordPress themes do.

---
+++ lib/theme.php
@@ -0,0 +1,6 @@
+<?php
+/**
+ * Sets up custom filters and actions for the theme.  This does things like sets up sidebars, menus, scripts, 
+ * and lots of other awesome stuff that WordPress themes do.
+ *
+ */
