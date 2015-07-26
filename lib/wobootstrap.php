d7eeee6ef6aad223dc4d7815e9a0c294c0e1fd7a
wobootstrap <bhoot.biswas@gmail.com>
2015-07-26 20:03:12 +0600

    Added code to load the Get the Image extension

--- lib/wobootstrap.php
+++ lib/wobootstrap.php
@@ -8,9 +8,15 @@
 
     /* Sets the path to the core framework classes directory. */
 	define( 'WOBOOTSTRAP_CLASSES', trailingslashit( WOBOOTSTRAP_DIR ) . 'classes' );
+
+    /* Sets the path to the core framework extensions directory. */
+    define( 'WOBOOTSTRAP_EXTENSIONS', trailingslashit( WOBOOTSTRAP_DIR ) . 'extensions' );
     
     /**
      * Register Custom Navigation Walker.
      */
     require_once( trailingslashit( WOBOOTSTRAP_CLASSES ) . 'wp_bootstrap_navwalker.php' );
+
+    /* Load the Get the Image extension if supported. */
+	require_once( trailingslashit( WOBOOTSTRAP_EXTENSIONS ) . 'get-the-image.php' );
 ?>
