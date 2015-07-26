d2e9e407c7e50ee26dcbbf91c22494e482c9b5ab
wobootstrap <bhoot.biswas@gmail.com>
2015-07-26 15:17:28 +0600

    Registered Footer Widget Area

--- functions.php
+++ functions.php
@@ -105,6 +105,16 @@ function wobootstrap_widgets_init() {
 		'before_title'  => '<h1 class="widget-title">',
 		'after_title'   => '</h1>',
 	) );
+
+    register_sidebar( array(
+		'name'          => esc_html__( 'Footer Widget Area', 'wobootstrap' ),
+		'id'            => 'sidebar-2',
+		'description'   => '',
+		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+		'after_widget'  => '</aside>',
+		'before_title'  => '<h1 class="widget-title">',
+		'after_title'   => '</h1>',
+	) );
 }
 add_action( 'widgets_init', 'wobootstrap_widgets_init' );
 
