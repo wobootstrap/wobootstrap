298fa5175cfbae640edd818c85b588f9d97a2851
wobootstrap <bhoot.biswas@gmail.com>
2015-07-26 18:03:01 +0600

    Deleted func 'wobootstrap_posted_on'

--- template-parts/content.php
+++ template-parts/content.php
@@ -9,11 +9,6 @@
     <div class="entry-wrap">
         <header class="entry-header">
             <?php the_title( sprintf( '<h1 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h1>' ); ?>
-            <?php if ( 'post' == get_post_type() ) : ?>
-            <div class="entry-meta">
-                <?php wobootstrap_posted_on(); ?>
-            </div><!-- .entry-meta -->
-            <?php endif; ?>
         </header><!-- .entry-header -->
         <div class="entry-content">
             <?php
